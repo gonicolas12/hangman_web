849465b45a2fb0a7099f1a4786535cd2d88b6691
Barros Celso <celso.barros@ynov.com>
2024-02-08 15:42:37 +0100

    Premiere page finalisée

---
+++ web/js/script.js
@@ -0,0 +1,42 @@
+/*// script.js
+alert("Jouer Hangman?");*/
+
+function togglepopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function toggleCredits() {
+    var popup = document.getElementById("popup-credits");
+    popup.classList.toggle("active");
+}
+
+function playAudio(audioID) {
+    var audio = document.getElementById(audioID);
+    audio.play();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var audio = document.getElementById("myAudio");
+    var musicImage = document.getElementById("musicImage");
+
+    // Ajout d'un gestionnaire d'événements pour le clic sur le bouton
+    toggleButton.addEventListener("click", function() {
+        // Vérification de l'état de lecture audio actuel
+        if (audio.paused) {
+            // Si la lecture est en pause, démarrer l'audio
+            audio.play();
+            // Changer l'image en musique activée
+            musicImage.src = "../image/musicOn.png";
+            musicImage.alt = "Music On";
+        } else {
+            // Si la lecture est en cours, mettre en pause l'audio
+            audio.pause();
+            // Changer l'image en musique désactivée
+            musicImage.src = "../image/musicOff.png";
+            musicImage.alt = "Music Off";
+        }
+    });
+});
+
+
+  
